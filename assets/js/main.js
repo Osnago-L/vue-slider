47d6e35647d7bd264ee91511acb74985fcea0aaf
Osnago-L <osna.brera@gmail.com>
2022-01-14 16:01:13 +0100

    created new database

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const data =[
+    {
+        'img':'assets/img/01.jpg',
+        'title':'Svezia',
+        'text':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+        'img':'assets/img/02.jpg',
+        'title':'Svizzera',
+        'text':'Lorem ipsum'
+    },
+    {
+        'img':'assets/img/03.jpg',
+        'title':'Gran Bretagna',
+        'text':'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        'img':'assets/img/04.jpg',
+        'title':'Germania',
+        'text':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+    },
+    {
+        'img':'assets/img/05.jpg',
+        'title':'Paradise',
+        'text':'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+    },
+];
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
